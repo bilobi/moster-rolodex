511d10b96dec128d08c426dd3dd5b42613da2e02
bilobi <bilalatmaca@gmail.com>
2020-06-15 00:54:11 +0300

    Updates

---
+++ precache-manifest.3ba4c9aaf2cbe42b8e1b3e4602ef7e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcb2e9ef93b2c2b2610d9c0f86ad1f8",
+    "url": "/monster-rolodex.git/index.html"
+  },
+  {
+    "revision": "d2bc044b10b5d3c65ec5",
+    "url": "/monster-rolodex.git/static/css/main.4e0932f7.chunk.css"
+  },
+  {
+    "revision": "7343f8aa0f91092c8dce",
+    "url": "/monster-rolodex.git/static/js/2.919d04c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex.git/static/js/2.919d04c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bc044b10b5d3c65ec5",
+    "url": "/monster-rolodex.git/static/js/main.c57b1cd5.chunk.js"
+  },
+  {
+    "revision": "201cb6fc884c62c72705",
+    "url": "/monster-rolodex.git/static/js/runtime-main.e2cc6812.js"
+  }
+]);
